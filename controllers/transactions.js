230b21d844db5e3c171755bc3640bd091c89e1fd
Jesse <jessean.infiltrator@gmail.com>
2022-10-28 21:50:08 -0700

    transactionsController.postTransaction implemented .findOneAndUpdate that's adaptable to whether the submitted transaction is involved with one or two accounts

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -52,6 +52,7 @@ module.exports = {
 
             ON UPDATING MULTIPLE SUBDOCUMENTS
             await User.findOneAndUpdate(
+            //     //the query
             //     {_id: dummyUser._id},
             //     //updates
             //     {
@@ -69,6 +70,21 @@ module.exports = {
             //         ]
             //     }
             // )
+
+            ON UPDATING A SINGLE SUBDOCUMENT
+            // // use .findOneAndUpdate command to save changes.
+            // await User.findOneAndUpdate({
+            //     "accounts._id": mongoose.Types.ObjectId(accountId)
+            // },
+            // {
+            //     $push: {
+            //         "transactions": newTransaction,
+            //         "accounts.$.transactions": mongoose.Types.ObjectId(newTransactionId)
+            //     },
+            //     $set: {
+            //         "accounts.$.currentBalance": newBalance
+            //     }
+            // })
 
             CATEGORY
             if user.categories does not have category, create it.
@@ -98,157 +114,154 @@ module.exports = {
             
             //set body and primary accountId
             const body = req.body
-            const primaryAccountId = req.params.accountId
             const dummyUser = req.user
+            
+            //get primary account object
+            // const primaryAccountIndex = dummyUser.accounts.findIndex(account => account._id == accountId)
+            const primaryAccountId = req.params.accountId
+            const primaryAccount = dummyUser.accounts.filter(account => account._id == primaryAccountId)[0]
+            
+            //initiate mongoose update action objects
+            const query = {}
+            const push = {}
+
+            // //CATEGORY & secondAccount
+            // // if user.categories does not have the incoming category, create it.
+            let cleanIncomingCategory = body.category.trim()
+            cleanIncomingCategory = cleanIncomingCategory[0].toUpperCase() + cleanIncomingCategory.slice(1).toLowerCase()
+            // console.log(cleanCategory)
+            // instantiate secondAccount variables
+            let secondAccountId = null
+            let secondAccount = null
+            // instantiate categoryId variable
+            let categoryId = null
+            // find the category by name using cleanCategory
+            const foundCategory = dummyUser.categories.filter(category => {
+                let cleanCategory = category.name[0].toUpperCase() + category.name.slice(1).toLowerCase()
+                return cleanCategory === cleanIncomingCategory
+            })[0]
+            console.log(foundCategory)
+            if (!foundCategory) { //if a category isn't found
+                //create a new category
+                const newCategory = new Category({
+                    name: cleanIncomingCategory
+                })
+                //add to user categories by
+                //adding categories: newCategory to mongoose $push update
+                push.categories = newCategory
+                categoryId = newCategory._id
+            } else { //if a category is found
+                if (foundCategory.account) { //if its account property isn't null
+                    secondAccountId = foundCategory.account.toString() //set secondAccountId and use it to pull up the secondAccount object
+                    console.log(typeof secondAccountId)
+                    // console.log(dummyUser.accounts.filter(account => account._id == '635b0c0ccc363d1950ba130f'))
+                    secondAccount = dummyUser.accounts.filter(account => account._id == secondAccountId)[0]
+                    console.log("secondAccount", secondAccount)
+                }
+                // // if category has an account attached, that account needs to
+                // // have the transaction id pushed to its transactions and have its
+                // // currentBalance incremented as well
+                categoryId = foundCategory._id
+            }
 
             //set accounting actions
             const accountingAction = body.accountingType
             const otherAction = (accountingAction === 'debit' ? 'credit' : 'debit')
 
-            //get primary account object
-            // const primaryAccountIndex = dummyUser.accounts.findIndex(account => account._id == accountId)
-            // const primaryAccount = dummyUser.accounts.filter(account => account._id == accountId)[0]
-
-            // 
-
-
-
-            // const newPush = {}
-
-            // //CATEGORY
-            // // if user.categories does not have category, create it.
-            // let cleanCategory = body.category.trim()
-            // let secondAccount = null
-            // let secondAccountId = null
-            // let secondAccountIndex = null
-            // let categoryId = null
-            // cleanCategory = cleanCategory[0].toUpperCase() + cleanCategory.slice(1).toLowerCase()
-            // cleanCategory = cleanCategory.trim()
-            // const foundCategory = user.categories.filter(category => category.name === cleanCategory)[0]
-            // if (!foundCategory) {
-            //     const newCategory = new Category({
-            //         name: cleanCategory
-            //     })
-            //     newPush.categories = newCategory
-            //     categoryId = newCategory._id
-            // } else {
-            //     if (foundCategory.account) {
-            //         secondAccountId = foundCategory.account
-            //         secondAccount = user.accounts.filter(account => account._id == secondAccountId)[0]
-            //     }
-            //     // // if category has an account attached, that account needs to
-            //     // // have the transaction id pushed to its transactions and have its
-            //     // // currentBalance incremented as well
-            //     categoryId = foundCategory._id
-            // }
-            // if (secondAccount) secondAccountIndex = user.accounts.findIndex(account => account._id == secondAccountId)
-
             // // // 1. instantiate new Transaction object
-            // const newTransaction = new Transaction({
-            //     [accountingAction]: accountId,
-            //     [otherAction]: categoryId,
-            //     amount: body.amount
-            // })
+            const newTransaction = new Transaction({
+                [accountingAction]: mongoose.Types.ObjectId(primaryAccountId),
+                [otherAction]: mongoose.Types.ObjectId(categoryId),
+                amount: body.amount
+            })
+            // add to user transactions by
+            // adding transactions: newTransaction to the mongoose $push update
+            push.transactions = newTransaction
 
-            // const newTransactionId = newTransaction._id
+            const newTransactionId = newTransaction._id
 
             // // // 2. push transactionId to .transactions array of, and 
             // // // increment each account that needs it
 
-            // const accounts = [primaryAccount, secondAccount]
+            // newPrimaryBalance will be used to 
+            let primaryAccountUpdate = null
+            let secondAccountUpdate = null
+            // set up primaryAccountUpdate to use increment/decrement
+            const updateAmount = Number(body.amount)
+            if (primaryAccount.balanceType === 'asset') {
+                //increment debit
+                //decrement credit
+                primaryAccountUpdate = (accountingAction === 'debit' ? updateAmount : -updateAmount)
+            } else {
+                //increment credit
+                //decrement debit
+                primaryAccountUpdate = (accountingAction === 'credit' ? updateAmount : -updateAmount)
+            }
 
-            // primaryAccount[`${accountingAction}s`].push(newTransactionId)
-            // const newPrimaryBalance = Number(primaryAccount.currentBalance)
-            // if (primaryAccount.balanceType === 'asset') {
+            let arrayFilters = null
+            let primaryAccountUpdateBalance = null
+            const update = {}
+            const inc = {}
+            
+            if (secondAccount) { //if secondAccount is found
+                // set up mongoose query object for multiple subdocuments
+                query._id = dummyUser._id
+                // set up mongoose update for multiple subdocument updates
+                // // each account should have the newTransactionId pushed to its
+                // // appropriate accounting array (debit/credit)
+                const primaryAccountPushString = `accounts.$[primary].${accountingAction}s`
+                const secondAccountPushString = `accounts.$[secondary].${otherAction}s`
+                push[primaryAccountPushString] = newTransaction._id
+                push[secondAccountPushString] = newTransaction._id
+                // // each account's currentBalance should be properly incremented/decremented
+                secondAccountUpdate = (primaryAccountUpdate < 0 ? -updateAmount : updateAmount)
+                primaryAccountUpdateBalance = {
+                    "accounts.$[primary].currentBalance": primaryAccountUpdate
+                }
+                const secondaryAccountUpdateBalance = {
+                    "accounts.$[secondary].currentBalance": secondAccountUpdate
+                }
+                update["$push"] = push
+                update["$inc"] = {}
+                update["$inc"]["accounts.$[primary].currentBalance"] = primaryAccountUpdate
+                update["$inc"]["accounts.$[secondary].currentBalance"] = secondAccountUpdate
+                // set up mongoose arrayFilters
+                const arrayFilters = {
+                    upsert: false,
+                    arrayFilters: [
+                        {"primary._id": mongoose.Types.ObjectId(primaryAccountId)},
+                        {"secondary._id": mongoose.Types.ObjectId(secondAccountId)}
+                    ]
+                }
 
-            // } else if (primaryAccount.balanceType === 'liability') {
-            // const dummyUser = req.user
-            // req.user.userName = "changed from jesse"
-            // user = dummyUser
-            // await user.save()
-            // }
-            // const firstAccountPush = `first account ${primaryAccount.name} ${accountingAction}`
-            // user.accounts[primaryAccountIndex][`${accountingAction}s`] = user.accounts[primaryAccountIndex][`${accountingAction}s`].concat(firstAccountPush)
-            // console.log(firstAccountPush)
-            // console.log(user.accounts[primaryAccountIndex][`${accountingAction}s`])
-            // if (secondAccount) {
-            //     const secondAccountPush = `second account ${secondAccount.name} ${otherAction}`
-            //     user.accounts[secondAccountIndex][`${otherAction}s`].push(secondAccountPush)
-            //     console.log(secondAccountPush)
-            // }
+                await User.findOneAndUpdate(
+                    //the query
+                    query,
+                    //updates
+                    update,
+                    //arrayFilters
+                    arrayFilters
+                )
+            } else { //if only primaryAccount is found
+                // set up mongoose update for a single subdocument update
+                query["accounts._id"] = mongoose.Types.ObjectId(primaryAccountId)
+                // // account should have newTransactionId pushed to its appropriate account (debit/credit)
+                const accountTransactionPushString = `accounts.$.${accountingAction}s`
+                push[accountTransactionPushString] = mongoose.Types.ObjectId(newTransactionId)
+                // // account's currentBalance should be properly incremented/decremented
+                primaryAccountUpdateBalance = {
+                    "accounts.$.currentBalance": primaryAccountUpdate
+                }
+                update["$push"] = push
+                update["$inc"] = primaryAccountUpdateBalance
 
-            // user.save().then(savedUser => {
-            //     savedUser === user
-            // })
-            // const actions = [accountingAction, otherAction]
-
-            // for (let i = 0; i < actions.length; i++) {
-            //     let foundAccount = user.accounts.filter(account => account._id == newTransaction[actions[i]])[0]
-            //     if (!foundAccount) {
-            //         const foundCategory = user.categories.filter(category => category._id == newTransaction[actions[i]])
-            //         foundAccount = user.accounts.filter(account => account._id == foundCategory.account)[0]
-            //     }
-            //     if (foundAccount) {
-            //         console.log(`pre changes`, foundAccount)
-            //         // push newTransactionId to debits/credits array
-            //         foundAccount[`${actions[i]}s`].push(newTransactionId)
-            //         // increment/decrement account's currentBalance
-            //         let newBalance = foundAccount.currentBalance
-            //         console.log(newBalance)
-            //         if (foundAccount.balanceType === 'asset') {
-            //             //increment debit
-            //             //decrement credit
-            //             newBalance = Number(newBalance) + (actions[i] === 'debit' ? Number(newTransaction.amount) : Number(-newTransaction.amount))
-            //         } else if (foundAccount.balanceType === 'liability') {
-            //             //increment credit
-            //             //decrement debit
-            //             newBalance = Number(newBalance) + (actions[i] === 'credit' ? Number(newTransaction.amount) : Number(-newTransaction.amount))
-            //         }
-            //         console.log(newBalance)
-            //         foundAccount.currentBalance = newBalance.toFixed(2)
-            //     }
-            //     console.log(`post changes`, foundAccount)
-            // }
-
-            // // use .findOneAndUpdate command to save changes.
-            // await User.findOneAndUpdate({
-            //     "accounts._id": mongoose.Types.ObjectId(accountId)
-            // },
-            // {
-            //     $push: {
-            //         "transactions": newTransaction,
-            //         "accounts.$.transactions": mongoose.Types.ObjectId(newTransactionId)
-            //     },
-            //     $set: {
-            //         "accounts.$.currentBalance": newBalance
-            //     }
-            // })
-
+                // // use .findOneAndUpdate command to save changes.
+                await User.findOneAndUpdate(
+                    query,
+                    update
+                )
+            }
             res.redirect("/profile")
-
-            //function for modifying account subdocuments
-            //inputs: transaction object, account id string
-            //outputs: account object
-            // function addTransactionToAccounts(transactionObj, accountId) {
-            //     const accountObj = user.accounts.filter(account => account._id == accountIdStr)[0]
-
-            //     // // 1. calculate new account balance using req.user.account
-            //     let newBalance = 0
-            //     if (targetAccount.balanceType === 'asset') {
-            //         if (accountingAction === 'debit') newBalance = currentBalance + transactionAmount
-            //         else newBalance = currentBalance - transactionAmount
-            //     } else {
-            //         if (accountingAction === 'credit') newBalance = currentBalance + transactionAmount
-            //         else newBalance = currentBalance - transactionAmount
-            //     }
-            //     newBalance = newBalance.toFixed(2)
-
-
-                
-            //     // // 2. push transactionId to account.transactions
-                
-            //     // // 3. increment/decrement account.currentBalance
-            // }
         } catch (err) {
             console.error(err)
             res.redirect("/profile")
